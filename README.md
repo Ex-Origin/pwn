bd6e0ae0198a498fff58c84ec671b59851fc64f2
Ex-Origin <2462148389@qq.com>
2020-11-29 19:17:42 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+## PWN Challenge
+
+Here will share the challenge that had been removed, and open source code, the expected writeup and user-submitted writeup.
--- user/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('../config.php');
-define('SELF_FILE', __FILE__);
-
-session_destroy();
-
-header('location:' . relative(SELF_FILE) . 'index.php');
-
-?>
