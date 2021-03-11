4b928a1e328bf9836041d522be4e0219253651d2
Ex-Origin <2462148389@qq.com>
2021-03-12 01:44:56 +0800

    Use vs2015 styles with highlight.js .

--- template/source_footer.php
+++ template/source_footer.php
@@ -3,7 +3,7 @@
 <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://cdn.staticfile.org/showdown/1.9.0/showdown.min.js"></script>
 
-<link rel="stylesheet" href="https://cdn.staticfile.org/highlight.js/9.13.1/styles/github.min.css">
+<link rel="stylesheet" href="https://cdn.staticfile.org/highlight.js/9.13.1/styles/vs2015.min.css">
 <script src="https://cdn.staticfile.org/highlight.js/9.13.1/highlight.min.js"></script>
 <script>hljs.initHighlightingOnLoad();</script>
 
